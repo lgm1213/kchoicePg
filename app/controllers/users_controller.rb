76ffd490f06120f23a351fcf4f79c6c31c53b26b
LGM1213 <lgm1213@gmail.com>
2017-04-13 14:39:17 -0400

    added footer and header and updated user model and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,33 @@
 class UsersController < ApplicationController
-  def new
-  end
 
   def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Karol's Choice"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :gender_cd)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :username, presence: true, length: { maximum: 30 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :gender_cd, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+  
+  #uses simple_enu gemm
+  as_enum :gender, :female => 1, :male => 0
+  
+  # Returns the hash digest of a given string
+  def User.digest(string)
+  	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+  	BCrypt::Password.create(string, cost: cost)
+  end
 end
