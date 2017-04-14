9d759bb985ced25b18d6085739d60c59ec46a422
LGM1213 <lgm1213@gmail.com>
2017-04-14 13:01:49 -0400

    added remember me function to sessions and created columns on user model to enable function and added check box on login page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,7 +6,15 @@ module SessionsHelper
 	
 	# returns the current logged in user (if any are logged in at all)
 	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
+		if (user_id = session[:user_id])
+			@current_user ||= User.find_by(id: session[:user_id])
+		elsif (user_id = cookies.signed[:user_id])
+			user = User.find_by(id: user_id)
+			if user && user.authenticated?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end
+		end
 	end
 
   # Returns true if a user is logged in, if not returns false
@@ -14,9 +22,31 @@ module SessionsHelper
   	!current_user.nil?
   end
 
+  def forget(user)
+  	user.forget
+  	cookies.delete(:user_id)
+  	cookies.delete(:remember_token)
+  end
+
+
   # Logs out the current user if logged in
   def log_out
+  	current_user && forget(current_user)
   	session.delete(:user_id)
   	@current_user = nil
   end
 end
+
+
+
+def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
