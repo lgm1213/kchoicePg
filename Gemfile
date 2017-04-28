b53df7d0de6598bda15788fae89ad13bf170274c
LGM1213 <lgm1213@gmail.com>
2017-04-28 18:57:35 -0400

    added pg_search gem, began adding search fields to User index, corrected syntax error in sessions controller

--- Gemfile
+++ Gemfile
@@ -53,3 +53,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'simple_enum'
 gem 'bootstrap-sass', '~> 3.3.6'
+
+# search function gem for PostGres
+gem 'pg_search'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   before_action :admin_user, only: [:destroy]
 
   def index
-    @users = User.where(activated: true && false).paginate(page: params[:page])
+    @users = User.where(activated: true && false)
+    #.paginate(page: params[:page])
   end
 
   def new
